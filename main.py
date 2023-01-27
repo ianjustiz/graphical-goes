f8b4cdb6382474db55bc977c38cdeea3de42aa61
ianjustiz <justiz.ian8@gmail.com>
2023-01-27 18:21:15 -0500

    added epoch time formatting for filename

--- main.py
+++ main.py
@@ -12,10 +12,49 @@ import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
 
-bucket_name = "noaa-goes16"
+import calendar
+import datetime
+from datetime import timedelta
+
+# if len(sys.argv) < 2:
+# 	print("Usage: ./plot.py <Current year> <Current day> <>")
+# 	sys.exit()
+
+# import_from = sys.argv[1]
+# export_to = sys.argv[2]
 
 
-def fetch_obj(year, day, hour, subdirectory="ABI-L2-MCMIPF"):
+def fetch_with_params(base_time=datetime.datetime.utcnow(), offset=12, bucket_name = "noaa-goes16", subdirectory="ABI-L2-MCMIPC"):
+    incrementer = base_time - timedelta(hours=offset)
+    
+    while fetch_obj(incrementer.year, int(incrementer.strftime("%j")), incrementer.hour, bucket_name, subdirectory) != -1:
+        incrementer = incrementer + timedelta(hours=1)
+
+
+def get_time_file(filename):
+    start_index = 1+filename.find('s')
+
+    # Throw -1 if s not found in given file
+    if start_index == -1:
+        return -1
+    
+    search_from = filename[start_index:]
+
+    # Slice substring to get time
+    year = search_from[0:4]
+    day = search_from[4:7]
+    hour = search_from[7:9]
+    minute = search_from[9:11]
+    second = search_from[11:13]
+
+    # Create new time object
+    date_form = datetime.datetime.strptime("{}-{}-{}-{}-{}".format(year,day,hour,minute,second) , "%Y-%j-%H-%M-%S")
+
+    # Return epoch time
+    return date_form.strftime("%s")
+
+
+def fetch_obj(year, day, hour, bucket_name, subdirectory):
     s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
     s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
     s3_bucket = s3.Bucket(bucket_name)
@@ -24,16 +63,23 @@ def fetch_obj(year, day, hour, subdirectory="ABI-L2-MCMIPF"):
 
     inc = 0
 
+    # If no objects matching parameters found, return -1.
+    if len(objects) == 0:
+        return -1
+    
     for obj in objects:
         if obj.key[-1] == "/":
             continue
 
+        # Call get_time_file for each file
+
+
         meta_data = s3_client.head_object(Bucket=bucket_name, Key=obj.key)
         total_length = int(meta_data.get('ContentLength', 0))
-        local_name = "{}-{}_{}_{}-{}.nc".format(subdirectory, year, day, hour, inc)
+        local_name = "{}-{}_{}.nc".format(bucket_name, subdirectory, get_time_file(obj.key))
         downloaded = 0
 
-        # draw progress bar
+        # Draw progress bar.
         def progress(chunk):
             nonlocal downloaded
             downloaded += chunk
@@ -51,27 +97,4 @@ def fetch_obj(year, day, hour, subdirectory="ABI-L2-MCMIPF"):
         print("\n")
 
 
-fetch_obj(2023, 25, 2)
-
-
-
-
-
-
-
-
-
-
-
-
-# g16nc = Dataset('OR_ABI-L1b-RadM1-M3C02_G16_s20171931811268_e20171931811326_c20171931811356.nc', 'r')
-# radiance = g16nc.variables['Rad'][:]
-# g16nc.close()
-# g16nc = None
-#
-# fig = plt.figure(figsize=(6,6),dpi=200)
-# im = plt.imshow(radiance, cmap='Greys_r')
-# cb = fig.colorbar(im, orientation='horizontal')
-# cb.set_ticks([1, 100, 200, 300, 400, 500, 600])
-# cb.set_label('Radiance (W m-2 sr-1 um-1)')
-# plt.show()
+print(get_time_file("OR_ABI-L2-MCMIPF-M6_G17_s20230050400320_e20230050409398_c20230050409542.nc"))
